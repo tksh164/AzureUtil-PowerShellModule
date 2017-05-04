30c05321ebcd050eaafdce5153fbeb31a524397c
Takeshi Katano <takatano@outlook.com>
2017-05-05 03:13:24 +0900

    Move implementation of Get-AzureUtilEmptyResourceGroup cmdlet to individual file.

---
+++ AzureUtil/EmptyResourceGroup.psm1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+Get the resource groups that not contains any resources.
+
+.DESCRIPTION
+Get the resource groups that not contains any resources.
+
+.EXAMPLE
+    Get-AzureUtilEmptyResourceGroup
+
+This example is get the all empty resource groups.
+
+.LINK
+PowerShell Gallery: https://www.powershellgallery.com/packages/AzureUtil/
+
+.LINK
+GitHub: https://github.com/tksh164/AzureUtil-PowerShellModule
+#>
+function Get-AzureUtilEmptyResourceGroup
+{
+    [OutputType([Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup])]
+    [CmdletBinding()]
+    param ()
+
+    # Login check.
+    try { [void](Get-AzureRMContext -ErrorAction Stop) } catch { throw }
+
+    # Create a array that contains non empty resource group names.
+    $nonEmptyResourceGroupNmaes = @()
+    Get-AzureRmResource |
+        Group-Object -Property 'ResourceGroupName' -NoElement |
+        ForEach-Object -Process { $nonEmptyResourceGroupNmaes += $_.Name }
+
+    # Lookup the empty resource group name from all resource group name using non-empty resource group name array.
+    Get-AzureRmResourceGroup |
+        Where-Object -FilterScript { $nonEmptyResourceGroupNmaes -notcontains $_.ResourceGroupName }
+}
