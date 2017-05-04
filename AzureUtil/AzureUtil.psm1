af15a320cdccf1191195983e59dbb009e62d58b3
Takeshi Katano <takatano@outlook.com>
2017-05-05 03:30:45 +0900

    Change implementation file name.

--- AzureUtil/AzureUtil.psm1
+++ AzureUtil/AzureUtil.psm1
@@ -8,4 +8,4 @@
 
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'EmptyResourceGroup.psm1' -Resolve)
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'NonAttachedManagedDisk.psm1' -Resolve)
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'NonAttachedNonManagedDisk.psm1' -Resolve)
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'NonAttachedUnmanagedDisk.psm1' -Resolve)
