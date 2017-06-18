899812fdcddd139470c1b722ed154ffcf8e3fbe9
Takeshi Katano <takatano@outlook.com>
2017-06-18 17:38:43 +0900

    Add help for Invoke-AzureUtilRestMethod cmdlet

--- AzureUtil/RestMethod.psm1
+++ AzureUtil/RestMethod.psm1
@@ -12,29 +12,82 @@ function PreventTokenExpire
 }
 
 <#
-    .SYNOPSIS
+.SYNOPSIS
+Sends an HTTP or HTTPS request as Azure REST API.
 
-    .DESCRIPTION
+.DESCRIPTION
+The Invoke-AzureUtilRestMethod cmdlet sends HTTP and HTTPS requests to Azure REST API service endpoints that returns structured data.
 
-    .PARAMETER Method
-    .PARAMETER Uri
-    .PARAMETER Body
+.PARAMETER Method
+Specifies the method used for the web request. This parameter is optional. The default value is "Get".
 
-    .EXAMPLE
+.PARAMETER Uri
+Specifies the URI of the Azure REST API to which the web request is sent.
 
-    .EXAMPLE
+.PARAMETER AdditionalHeaders
+Specifies the additional headers of the web request. Enter a hash table or dictionary. This parameter is optional.
 
-    .EXAMPLE
+.PARAMETER ContentType
+Specifies the content type of the web request. This parameter is optional. The default value is "application/json".
 
-    .LINK
-    PowerShell Gallery: https://www.powershellgallery.com/packages/AzureUtil/
+.PARAMETER Body
+Specifies the body of the web request. The body is the content of the web request that follows the headers. This parameter is optional.
 
-    .LINK
-    GitHub: https://github.com/tksh164/AzureUtil-PowerShellModule
+.EXAMPLE
+    PS C:\>$result = Invoke-AzureUtilRestMethod -Uri 'https://management.azure.com/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourcegroups/Sample-RG?api-version=2017-05-10'
 
-    .LINK
+    PS C:\>$result.Content
+    {"id":"/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/Sample-RG","name":"Sample-RG","location":"japaneast","properties":{"provisioningState":"Succeeded"}}
 
-    .LINK
+    PS C:\>ConvertFrom-Json $result.Content | Format-Custom
+    class PSCustomObject
+    {
+      id = /subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/Sample-RG
+      name = Sample-RG
+      location = japaneast
+      properties = 
+        class PSCustomObject
+        {
+          provisioningState = Succeeded
+        }
+    }
+
+---- Example Description ----
+In this example, get the "Sample-RG" resource group information using Azure REST API. You can find this REST API details at https://docs.microsoft.com/en-us/rest/api/resources/resourcegroups#ResourceGroups_Get.
+
+.EXAMPLE
+    PS C:\>$result = Invoke-AzureUtilRestMethod -Method Head -Uri 'https://management.azure.com/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourcegroups/Sample-RG?api-version=2017-05-10'
+
+    PS C:\>$result.StatusCode
+    204
+
+---- Example Description ----
+In this example, checks whether the "Sample-RG" resource group exists using Azure REST API. This REST API is return the 204 as HTTP status code if that resource group is exists. You can find this REST API details at https://docs.microsoft.com/en-us/rest/api/resources/resourcegroups#ResourceGroups_CheckExistence.
+
+.EXAMPLE
+    PS C:\>$body = @'
+    {
+        "resources": [ "*" ],
+        "options": "IncludeParameterDefaultValue, IncludeComments"
+    }
+    '@
+
+    PS C:\>$result = Invoke-AzureUtilRestMethod -Method Post -Uri 'https://management.azure.com/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourcegroups/Sample-RG/exportTemplate?api-version=2017-05-10' -Body $body
+
+    PS C:\>$result.Content
+    {"template":{"$schema":"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#","contentVersion":"1.0.0.0","parameters":{"storageAccounts_abcd1234_name":{"defaultValue":"abcd1234","type":"String"}},"variables":{},"resources":[{"comments":"Generalized from resource: '/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/sample-rg/providers/Microsoft.Storage/storageAccounts/abcd1234'.","type":"Microsoft.Storage/storageAccounts","sku":{"name":"Standard_LRS","tier":"Standard"},"kind":"Storage","name":"[parameters('storageAccounts_abcd1234_name')]","apiVersion":"2016-01-01","location":"japaneast","tags":{},"scale":null,"properties":{},"dependsOn":[]}]}}
+
+---- Example Description ----
+In this example, capture the "Sample-RG" resource group as a template. You can find this REST API details at https://docs.microsoft.com/en-us/rest/api/resources/resourcegroups#ResourceGroups_ExportTemplate.
+
+.LINK
+PowerShell Gallery: https://www.powershellgallery.com/packages/AzureUtil/
+
+.LINK
+GitHub: https://github.com/tksh164/AzureUtil-PowerShellModule
+
+.LINK
+Azure REST API Reference: https://docs.microsoft.com/en-us/rest/api/
 #>
 function Invoke-AzureUtilRestMethod
 {
